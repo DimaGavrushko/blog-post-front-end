fab32164d4432528d5c45a9ac9ff7523104d12af
DimaGavrushko <gavrushko98@gmail.com>
2019-11-25 15:02:46 +0300

    fix: error with https

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,6 @@
 const getApiURL = () => {
   if (process.env.NODE_ENV === "production") {
-    return "http://api-blogpost.herokuapp.com";
+    return "https://api-blogpost.herokuapp.com";
   }
 
   return "http://localhost:8001";
