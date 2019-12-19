e0ea479434bd9c8454ef56a4701413458979efd3
Dmitry <gavrushko98@gmail.com>
2019-12-19 22:04:06 +0300

    refactor

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 import Notification from "../Notification";
